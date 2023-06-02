8778e08f9c20fecf40d06816e502b1604f6dd17a
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-06-02 21:49:05 +0530

    minor changes

--- Session5/model.py
+++ Session5/model.py
@@ -103,3 +103,14 @@ def training(model, device, num_epochs, train_loader, test_loader, optimizer, cr
         scheduler.step()
 
     return train_losses, test_losses, train_acc, test_acc, test_incorrect_pred
+
+def getTrainingTestPlots(train_losses, test_losses, train_acc, test_acc):
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
