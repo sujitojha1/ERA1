e8771ec203f882f42ecf8aeb4bd945963c3f2558
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-06-02 21:35:34 +0530

    small change

--- Session5/utils.py
+++ Session5/utils.py
@@ -21,11 +21,11 @@ test_transforms = transforms.Compose([
 def getDataLoader(batch_size = 512):
    
     train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
-    test_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+    test_data = datasets.MNIST('../data', train=False, download=True, transform=test_transforms)
 
-    kwargs = {'batch_size': batch_size, 'shuffle': False, 'num_workers': 2, 'pin_memory': True}
+    kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
 
-    test_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
     train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
     
     return train_loader, test_loader
