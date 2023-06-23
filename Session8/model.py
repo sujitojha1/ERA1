65efae4d605f8e5f280b027b43a034105cf4dea8
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-06-23 22:51:42 +0530

    mino changes

--- Session8/model.py
+++ Session8/model.py
@@ -41,7 +41,7 @@ class Net(nn.Module):
 
         # CONVOLUTION BLOCK 2
         self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.ReLU(),
             normalizationFx(normalizationMethod,64),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, groups=64, bias=False),
@@ -55,6 +55,11 @@ class Net(nn.Module):
             normalizationFx(normalizationMethod,32),
             #nn.Dropout(dropout_value)
         ) 
+
+        self.shortcut1 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=1, stride=1, padding=0, bias=False),
+            normalizationFx(normalizationMethod,32),
+        )
 
         # CONVOLUTION BLOCK 3       
         self.convblock7 = nn.Sequential(
@@ -70,7 +75,7 @@ class Net(nn.Module):
             #nn.Dropout(dropout_value)
         ) #o/p size = 64*8*8 RF = 26
 
-        self.shortcut1 = nn.Sequential(
+        self.shortcut2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=1, stride=1, padding=0, bias=False),
             normalizationFx(normalizationMethod,64),
         )
@@ -91,12 +96,12 @@ class Net(nn.Module):
         x4 = self.transitionblock(x3)
 
         x5 = self.convblock4(x4)
-        x6 = x5 + x4
+        x6 = x5 + self.shortcut1(x4)
 
         x7 = self.transitionblock(x6)
 
         x8 = self.convblock7(x7)
-        x9 = x8 + self.shortcut1(x7)
+        x9 = x8 + self.shortcut2(x7)
 
         out = self.gap(x9)        
         out = out.view(out.size(0), -1)
