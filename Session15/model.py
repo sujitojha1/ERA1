40f26f069703f42f7952dc4e79609c74050ea447
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-08-31 22:23:18 +0530

    layer normalization added

---
+++ Session15/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import mail
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # alpha is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(1)) # bias is a learnable parameter
+
+    def forward(self, x):
+        # x : (batch, seq_len, hidden_size)
+        # keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean)/(std+self.eps) + self.bias
+    
+    
