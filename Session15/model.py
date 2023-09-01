14ec922b2e818980617d681b8ecf84a7563540e2
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-01 11:05:31 +0530

    positional embedding

--- Session15/model.py
+++ Session15/model.py
@@ -44,3 +44,34 @@ def InputEmbeddings(nn.Module):
         # Multiply by sqrt(d_model) to scale the embeddings according to the paper
         return self.embedding(x) * math.sqrt(self.d_model)
     
+class PositionalEmbedding(nn.Module):
+
+    def __init__(self, d_model:int, seq_len:int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+
+        # create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+
+        # Create a vector of shape (seq_len)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
+
+        # Create a vector of shape (d_model)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        # apply sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term) 
+
+        #apply cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        # add a batch dimenstion to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
+    def forward(self,x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq_len, d_model)
+        return self.dropout(x)
