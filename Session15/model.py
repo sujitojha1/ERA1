8cdd00060c214e323de9e66b58de28c30e4e8ae8
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-06 19:54:47 +0530

    encoder block

--- Session15/model.py
+++ Session15/model.py
@@ -153,3 +153,16 @@ class EncoderBlock (nn.Module):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
         x = self.residual_connections[1](x, self.feed_forward_block)
         return x
+
+
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None: 
+        super().__init__() 
+        self.layers = layers
+        self.norm = LayerNormalization()
+    
+    def forward(self, x, mask): 
+        for layer in self.layers:
+            x = layer(x, mask) 
+        
+        return self.norm(x)
