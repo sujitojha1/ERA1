2268bc63031b602c8acff77b6d88b54e5f98d297
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-06 20:04:47 +0530

    decoder added

--- Session15/model.py
+++ Session15/model.py
@@ -143,7 +143,7 @@ class MultiHeadAttentionBlock(nn.Module):
         return self.w_o(x)
 
 class EncoderBlock (nn.Module):
-    def __init__(self, self_attention_block: MultiReadAttentionBlock, feed_forward_block: FeedForwardBlock):
+    def __init__(self, self_attention_block: MultiReadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout:float = 0.1):
         super().__init__()
         self.self_attention_block = self_attention_block
         self.feed_forward_block = feed_forward_block
@@ -165,4 +165,28 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask) 
         
+        return self.norm(x)
+
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout:float = 0.1):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block 
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.self attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask)
+        x = self.residual_connections[2](x, self.feed_forward_block) 
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.Modulelist) -> None: 
+        super().__init__()
+        self.layers layers
+        self.norm = LayerNormalization()
+    def forward(self, x, encoder_output, src_mask, tgt_mask): 
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask) 
         return self.norm(x)
