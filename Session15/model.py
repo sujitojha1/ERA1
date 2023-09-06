9316e65d90049f7cb2c0cc4d737cdefdbb798fae
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-06 20:06:09 +0530

    added projection layer

--- Session15/model.py
+++ Session15/model.py
@@ -190,3 +190,12 @@ class Decoder(nn.Module):
         for layer in self.layers:
             x = layer(x, encoder_output, src_mask, tgt_mask) 
         return self.norm(x)
+
+class ProjectionLayer (nn.Module):
+    def __init__(self, d_model, vocab_size)-> None: 
+        super().__init__() 
+        self.proj = nn.Linear(d_model, vocab_size)
+    
+    def forward(self, x)-> None:
+        # (batch, seq_len, d_model)-> (batch, seq_len, vocab_size) 
+        return torch.log_softmax (self.proj(x), dim = -1)
