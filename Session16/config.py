11cad589ab76400883a62ec2e231e01be7770bda
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-08 14:49:09 +0530

    minor changes

--- Session16/config.py
+++ Session16/config.py
@@ -5,7 +5,7 @@ def get_config():
         "batch_size": 2048*2,
         "num_epochs": 20,
         "lr": 10**-3,
-        "seq_len": 160,
+        "seq_len": 500,
         "d_model": 512,
         "lang_src": "en",
         "lang_tgt": "fr",
