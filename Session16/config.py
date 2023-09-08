5e07b3a1083334133178e9111317aa6235bba83d
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-08 14:33:56 +0530

    updated config

--- Session16/config.py
+++ Session16/config.py
@@ -12,7 +12,7 @@ def get_config():
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": False,
-        "tokenizer_file": "tokenizer_{0}.json"
+        "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "rus/tmodel_param_grad_zero",
     }
 
