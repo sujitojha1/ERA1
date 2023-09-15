60764c13eedec618f0cc992494f089a0e6f0db1a
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-15 22:09:22 +0530

    inor changes

--- Session15/lit_translation.py
+++ Session15/lit_translation.py
@@ -1,2 +1,11 @@
 from lightning import LightningModule
 
+class TransformerPredictor(LightningModule):
+    def __init__(
+            self,
+
+            ):
+        
+        super().__init__()
+        self.save_hyperparameter()
+        self.model = 
