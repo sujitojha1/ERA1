a31c22225d1cc83818ee62d2f546fa2c592f6d1e
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-09-22 09:30:50 +0530

    minor changes

--- Session18/model.py
+++ Session18/model.py
@@ -34,7 +34,7 @@ class ExpandingBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,padding=1,bias=False)
+        self.conv1 = nn.Conv2d(in_channels + out_channels, out_channels, kernel_size=3,padding=1,bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu1 = nn.ReLU(inplace=True)
 
