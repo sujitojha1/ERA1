9235ade1bc1b90ef3dc5601217733656d5218d7e
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2024-01-03 08:43:24 +0530

    Update model.py

--- Capstone/model.py
+++ Capstone/model.py
@@ -1,1 +1,14 @@
+from __future__ import annotations
 
+import math
+from dataclasses import dataclass, field
+from typing import Any, Dict, Optional, Tuple, Union
+
+import torch
+import torch.nn as nn
+from einops import rearrange, repeat
+from transformers import PretrainedConfig, PreTrainedModel
+from transformers.activations import ACT2FN
+from transformers.modeling_outputs import CausalLMOutputWithPath
+
+from .configuration_phi import PhiConfig
